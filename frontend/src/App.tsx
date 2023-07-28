7cedc49a7387f823d5cbd205f4bb2e6a135b210d
AnshKathpal <storage.2.3107@gmail.com>
2023-07-28 13:32:40 +0530

    day-1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { NavBar } from "./Components/NavBar";
+import { MainRoutes } from "./Routes/MainRoutes";
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      <MainRoutes />
     </div>
   );
 }
