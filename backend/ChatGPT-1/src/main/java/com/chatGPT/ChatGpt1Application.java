9173ac7144b69d19fa46dc192879a48164d5548e
Abdul <abdulrashid06>
2023-07-28 19:59:37 +0530

    backend

---
+++ backend/ChatGPT-1/src/main/java/com/chatGPT/ChatGpt1Application.java
@@ -0,0 +1,13 @@
+package com.chatGPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGpt1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGpt1Application.class, args);
+	}
+
+}
