5b2665e70003f2f0d09a6510f1ad1f0f19e0f66d
Abdul <abdulrashid06>
2023-07-29 12:07:21 +0530

    code fix

---
+++ backend/ChatGPT-1/src/main/java/com/chatGPT/config/Config.java
@@ -0,0 +1,12 @@
+package com.chatGPT.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Configuration
+@CrossOrigin("*")
+public class Config {
+	
+	
+
+}
